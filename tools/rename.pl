55eb2fef52b25885edf4c3128e708ee9d30828c9
Hukanzen <qazxswed1934@gmail.com>
2017-01-13 18:16:51 +0900

    qiita

--- tools/rename.pl
+++ tools/rename.pl
@@ -1,15 +1,16 @@
-﻿$stat='mp4';
+﻿#!/usr/bin/env perl
+
+#Current Directoryにある指定した拡張子の、ファイル名の空白をアンダーバーに全置換。
+
+#拡張子指定
+$stat='mp4';
 $fl='*'.'.'.$stat.'*';
 
 @file=glob $fl;
 
-$i=0;
 foreach(@file){
 	$new=$_;
-	#$new=~s!(\.$stat\.\d*)!!g;
 	$new=~s!\s!_!g;
-	#$new=$new."_".$i.".".$stat;
 	print $_."-".$new."\n";
 	system("mv \"$_\" $new");
-	$i++;
 }
--- tools/split.pl
+++ tools/split.pl
@@ -17,27 +17,38 @@ my $rf=$of.$of_ka;
 my $split_sec=20*60;
 
 # ffmpeg コマンド 
+# -i:入力ファイル
 my $ffmpeg="/opt/ffmpeg_build/bin/ffmpeg -i $rf";
 
 # 元動画ファイルの長さ
 my $max_sec=&count_time;
 
 for(my $i=0,$j=0;;$i+=$split_sec,$j++){
+	#元動画ファイルよりも後ろの部分を切り取ろうとしたら、終了
 	if($i>$max_sec+1){
 		last;
 	}
 
+	#オプション
+	#配列にしてあるので、好みで変更できる
+	#-c :codecのコピー
+	#-ss:指定した時間から変換を始める
+	#-t :指定秒変換する
 	my @option1=("-c copy","-ss $i","-t $split_sec");
 
 	my $option=&mkop(\@option1);
 
+	#コマンド作成
 	$exe=$ffmpeg.$option;
+	#出力ファイル
 	$exe.=" $of"."_".$j.$of_ka;
 
+	#コマンド実行
 	print $exe."\n";
 	print "system=".system($exe)."\n";
 }
 
+#オプション作成
 sub mkop{
 	my ($op)=@_;
 	my $cmd="";
@@ -49,6 +60,7 @@ sub mkop{
 	return $cmd;
 }
 
+#動画の長さを取得
 sub count_time{
 	my $from_time=`$ffmpeg 2>&1 | grep Duration |cut -c 13-20`;
 	my @time=split(/:/,$from_time);
